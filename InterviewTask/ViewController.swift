e99c22be99b64c765d605f4a8deb2f31283b9fb3
Rita <ritaasryan2015@gmail.com>
2021-12-10 18:19:37 +0400

    Initial Commit

---
+++ InterviewTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InterviewTask
+//
+//  Created by Rita Asryan on 12/10/21.
+//  Copyright © 2021 Rita Asryan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
